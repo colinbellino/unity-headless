bcec2e0beb5a8c4f3f883a3eaf50fc3cf45460b5
Colin Bellino <bellino.colin@gmail.com>
2020-01-08 09:27:16 +0100

    ✨ Send toggle signals only when the status changed

--- Assets/Code/Core/Entities/PressureActivation.cs
+++ Assets/Code/Core/Entities/PressureActivation.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Greed.UnityWrapper;
 using Zenject;
 
@@ -9,6 +10,10 @@ namespace Greed.Core
 		private readonly SignalBus _signalBus;
 		private readonly IEntity _entity;
 		private readonly string _colliderTag;
+		private readonly int _requiredActivators = 1;
+
+		private readonly List<IEntity> _activators = new List<IEntity>();
+		private bool _activated = false;
 
 		public PressureActivation(
 			SignalBus signalBus,
@@ -24,11 +29,13 @@ namespace Greed.Core
 		public void Initialize()
 		{
 			_entity.TriggerEntered += TriggerEntered;
+			_entity.TriggerExited += TriggerExited;
 		}
 
 		public void Dispose()
 		{
 			_entity.TriggerEntered -= TriggerEntered;
+			_entity.TriggerExited -= TriggerExited;
 		}
 
 		private void TriggerEntered(ICollider2D collider)
@@ -39,8 +46,32 @@ namespace Greed.Core
 			}
 
 			var activator = collider.GameObject.GetComponentInParent<IEntity>();
-			var signal = new ButtonToggledSignal { Target = _entity, Activator = activator };
-			_signalBus.Fire(signal);
+			_activators.Add(activator);
+			TrySendToggleSignal();
+		}
+
+		private void TriggerExited(ICollider2D collider)
+		{
+			if (!collider.CompareTag(_colliderTag))
+			{
+				return;
+			}
+
+			var activator = collider.GameObject.GetComponentInParent<IEntity>();
+			_activators.Remove(activator);
+			TrySendToggleSignal();
+		}
+
+		private void TrySendToggleSignal()
+		{
+			var activated = _activators.Count >= _requiredActivators;
+			if (_activated != activated)
+			{
+				var signal = new ButtonToggledSignal { Target = _entity };
+				_signalBus.Fire(signal);
+
+				_activated = activated;
+			}
 		}
 	}
 }
